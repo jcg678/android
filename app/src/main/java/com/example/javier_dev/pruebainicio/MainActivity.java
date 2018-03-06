827687e16c63b557ea0c254a060866a1c7a07f6a
jcg678 <jcg678@hotmail.es>
2018-03-06 11:38:02 +0100

    ciclo de la actividad

--- app/src/main/java/com/example/javier_dev/pruebainicio/MainActivity.java
+++ app/src/main/java/com/example/javier_dev/pruebainicio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.javier_dev.pruebainicio;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
+        // /Activity activity = this;
+        /*Context context =this;
+        Test.myToast(context);*/
+
+
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause(){
+
+        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show();
+        super.onPause();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show();
+
     }
 }
---
+++ app/src/main/java/com/example/javier_dev/pruebainicio/Test.java
@@ -0,0 +1,15 @@
+package com.example.javier_dev.pruebainicio;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by JAVIER-DEV on 06/03/2018.
+ */
+
+public class Test {
+    public static void myToast(Context context){
+        Toast.makeText(context,"Prueba", Toast.LENGTH_SHORT).show();
+    }
+
+}
