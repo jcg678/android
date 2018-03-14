8b3a4549292058431fd516a9ad0950154cdc5780
jcg678 <jcg678@hotmail.es>
2018-03-14 14:02:27 +0100

    button + toast

--- app/src/main/java/com/example/javier_dev/pruebainicio/MainActivity.java
+++ app/src/main/java/com/example/javier_dev/pruebainicio/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,45 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
-        // /Activity activity = this;
-        /*Context context =this;
-        Test.myToast(context);*/
-
-
-
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show();
-    }
-    @Override
-    protected void onPause(){
-
-        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show();
-        super.onPause();
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show();
-    }
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show();
-    }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show();
+    public  void miMetodo(View v){
+        int a = 4;
+        Toast.makeText(MainActivity.this,"hola",Toast.LENGTH_SHORT).show();
 
     }
+
+
 }
